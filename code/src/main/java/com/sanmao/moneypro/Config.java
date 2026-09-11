f74ba877739de5bfa93175e461586af528a0a02f
leguandesanmao <q13795122935@88.com>
2026-09-11 17:08:00 +0800

    Update the config file

--- code/src/main/java/com/sanmao/moneypro/Config.java
+++ code/src/main/java/com/sanmao/moneypro/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.sanmao.moneypro;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
--- code/src/main/java/com/sanmao/moneypro/ExampleMod.java
+++ code/src/main/java/com/sanmao/moneypro/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.sanmao.moneypro;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
